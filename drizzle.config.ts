e12e6dd42beeeb7ca16a39ad26b62993c745795d
User Name <mckylesingh71@gmail.com>
2025-05-01 16:32:45 +0200

    Successfully created db and tables  through migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -10,6 +10,6 @@ export default defineConfig({
    schema: "./db/schema.ts",
    dialect: "postgresql",
    dbCredentials: {
-      url: process.env.NEON_DATBASE_URL!
+      url: process.env.NEON_DATABASE_URL!
    },
 });
