087ba7bf3708f5050c19fadae183480713e4a031
User Name <mckylesingh71@gmail.com>
2025-05-01 20:47:49 +0200

    Designed Login form

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -1,5 +1,80 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { emailSchema } from "@/validation/emailSchema";
+import { passwordSchema } from "@/validation/passwordSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+   email: emailSchema,
+   password: passwordSchema
+})
+
 export default function Login() {
+   const form = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+         email: "",
+         password: "",
+      }
+   });
+
+   const handleSubmit = async (data: z.infer<typeof formSchema>) => {
+      
+   }
+
    return (
-      <div>Login</div>
-   )
-};
+      <main className="flex justify-center items-center min-h-screen">
+          <Card className="w-[350px]">
+            <CardHeader>
+               <CardTitle>Login</CardTitle>
+               <CardDescription>Login to your account</CardDescription>
+            </CardHeader>
+            <CardContent>
+               <Form {...form}>
+                  <form
+                     onSubmit={form.handleSubmit(handleSubmit)}
+                     className="flex flex-col gap-2">
+                     <fieldset disabled={form.formState.isSubmitting} className="flex flex-col gap-2" >
+                        <FormField
+                           control={form.control}
+                           name="email"
+                           render={({ field }) => (
+                              <FormItem>
+                                 <FormLabel>
+                                    Email
+                                 </FormLabel>
+                                 <FormControl>
+                                    <Input {...field} type="email" />
+                                 </FormControl>
+                                 <FormMessage />
+                              </FormItem>
+                           )} />
+                        <FormField
+                           control={form.control}
+                           name="password"
+                           render={({ field }) => (
+                              <FormItem>
+                                 <FormLabel>
+                                    Password
+                                 </FormLabel>
+                                 <FormControl>
+                                    <Input {...field} type="password" />
+                                 </FormControl>
+                                 <FormMessage />
+                              </FormItem>
+                           )} />
+                        <Button type="submit">Login</Button>
+                     </fieldset>
+                  </form>
+               </Form>
+            </CardContent>
+         </Card>
+      </main>
+   );
+}
